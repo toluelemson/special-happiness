5d63d5cd59ab2756a0960069db9560eeafc91d8f
elemson <toluwanielemosho@gmail.com>
2025-01-20 08:02:42 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PagingComponent } from './paging-component/paging-component.component';
+import {CreditCardInputComponent} from './credit-card-input/credit-card-input.component';
+import {PasswordComponent} from './password/password.component';
+import {StandingOrderOverviewComponent} from './standing-order-overview/standing-order-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'paging/1', pathMatch: 'full' },
+  { path: 'paging/:page', component: PagingComponent },
+  { path: 'credit-card-input', component: CreditCardInputComponent },
+  { path: 'password', component: PasswordComponent },
+  { path: 'standing-order-overview', component: StandingOrderOverviewComponent },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {enableProdMode} from '@angular/core';
+
+const environment = { production: false };
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
